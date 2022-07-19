286ae085451d41c83c33c65c0f29136ed41900d1
Robert Krawczyk <rkrawczyk@mail.bradley.edu>
2022-07-19 18:21:26 -0500

    added c++ (proj cant be compiled)

---
+++ Source/CombatGraph/Public/RAnimUtility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimationBlueprintLibrary.h"
+#include "RAnimUtility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATGRAPH_API URAnimUtility : public UAnimationBlueprintLibrary
+{
+	GENERATED_BODY()
+	
+};
