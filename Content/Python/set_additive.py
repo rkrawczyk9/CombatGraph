e1cd3612c0adfabd0ad3bef711e187afac0667c3
rkrawczyk9 <42722696+rkrawczyk9@users.noreply.github.com>
2022-08-05 10:54:46 -0500

    upper body aim with AO

--- Content/Python/set_additive.py
+++ Content/Python/set_additive.py
@@ -6,9 +6,9 @@ import unreal
 
 
 def main():
-    set_additive(additive=unreal.AdditiveAnimationType.AAT_NONE)
+    #set_additive(additive=unreal.AdditiveAnimationType.AAT_NONE)
     # set_additive(additive=unreal.AdditiveAnimationType.AAT_LOCAL_SPACE_BASE)
-    # set_additive(additive=unreal.AdditiveAnimationType.AAT_ROTATION_OFFSET_MESH_SPACE)
+    set_additive(additive=unreal.AdditiveAnimationType.AAT_ROTATION_OFFSET_MESH_SPACE)
     # set_additive(additive=unreal.AdditiveAnimationType.AAT_ROTATION_OFFSET_MESH_SPACE,
     #              ref_pose_type = unreal.AdditiveBasePoseType.ABPT_ANIM_FRAME,
     #              ref_pose = "/Game/Animations/pose_BasePose.pose_BasePose",
@@ -24,16 +24,16 @@ def set_additive(anim_seqs = None, additive = None, ref_pose_type = None, ref_po
     ref_pose_type:  None or unreal.AdditiveBasePoseType
     ref_pose:       None or str path to animseq or unreal.AnimSequence
     """
-    
+
     log = "set_additive.py: Successfully set "
-    
+
     # Check 'additive' param
     if additive:
         if not isinstance(additive, unreal.AdditiveAnimationType):
             unreal.log_error("set_additive.py: {} is not an unreal.AdditiveAnimationType".format(additive))
             return
         log += "additive = " + str_aat(additive)
-    
+
     # Check 'ref_pose_type' param
     if ref_pose_type:
         if not isinstance(ref_pose_type, unreal.AdditiveBasePoseType):
@@ -43,7 +43,7 @@ def set_additive(anim_seqs = None, additive = None, ref_pose_type = None, ref_po
         if additive:
             log += ", "
         log += "base pose type = " + str_abpt(ref_pose_type)
-    
+
     # Check 'ref_pose' param
     ref_pose_seq = ref_pose
     if ref_pose:
@@ -60,11 +60,11 @@ def set_additive(anim_seqs = None, additive = None, ref_pose_type = None, ref_po
         elif not isinstance(ref_pose, unreal.AnimSequence):
             unreal.log_error("set_additive.py: {} is not an AnimSequence or a path".format(ref_pose))
             return
-        
+
         if additive or ref_pose_type:
             log += ", "
         log += "base pose = " + ref_pose_seq.get_name()
-    
+
     if ref_pose_index is not None:
         if not isinstance(ref_pose_index, int):
             unreal.log_error("set_additive.py: {} is not an int".format(ref_pose_index))
@@ -85,7 +85,7 @@ def set_additive(anim_seqs = None, additive = None, ref_pose_type = None, ref_po
         else:
             unreal.log_error("set_additive.py: Not all selected are AnimSequences")
             return
-        
+
 
     num_tasks = len(anim_seqs)
     progress_bar_text = "Setting additiveness for {} anim sequences".format(num_tasks)
@@ -108,12 +108,12 @@ def set_additive(anim_seqs = None, additive = None, ref_pose_type = None, ref_po
                 anim_seq.set_editor_property("ref_pose_seq", ref_pose_seq)
             if ref_pose_index:
                 anim_seq.set_editor_property("ref_frame_index", ref_pose_index)
-            
+
             log += "{}, ".format(anim_seq.get_name())
-    
+
     log = log[:-2] + "]" # Remove last comma and add bracket
     unreal.log(log)
-            
+
 
 def str_aat(additive_anim_type):
     """
@@ -133,7 +133,7 @@ def str_abpt(additive_base_pose_type):
 
     # TODO
     return str(additive_base_pose_type)
-    
+
 
 
 if __name__ == "__main__":
